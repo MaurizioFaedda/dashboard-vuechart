877e7d662e604e9f60b642dbafad114fa717837b
Maurizio <maurizio.faedda1@gmail.com>
2021-03-26 16:40:19 +0100

    Install Vue-Bootstrap

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,19 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div>
+      <b-button>Button</b-button>
+      <b-button variant="danger">Button</b-button>
+      <b-button variant="success">Button</b-button>
+      <b-button variant="outline-primary">Button</b-button>
+    </div>
+    <div>
+      <b-button v-b-modal.modal-1>Launch demo modal</b-button>
+
+      <b-modal id="modal-1" title="BootstrapVue">
+        <p class="my-4">Hello from modal!</p>
+      </b-modal>
+    </div>
   </div>
 </template>
 
